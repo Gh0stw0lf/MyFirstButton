0c6c1ae2bb4d2472731724b0aea001816c4c02e1
Jorge Guevara <jorge.guevara@engineerefficiency.com>
2021-10-26 09:58:31 -0500

    Initial Commit

---
+++ MyFirstButton/MyFirstButtonApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyFirstButtonApp.swift
+//  MyFirstButton
+//
+//  Created by Jorge Guevara on 10/26/21.
+//
+
+import SwiftUI
+
+@main
+struct MyFirstButtonApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
